8a04262f5a82e63b02aeb4938800e13c921f44bb
David Testé <david.teste@zama.ai>
2023-05-22 17:38:11 +0200

    chore(ci): store benchmarks results under new zama standard

--- benches/ntt.rs
+++ benches/ntt.rs
@@ -1,4 +1,9 @@
-use std::ops::{Add, Mul, Sub};
+use serde::Serialize;
+use std::{
+    fs,
+    ops::{Add, Mul, Sub},
+    path::PathBuf,
+};
 
 use concrete_ntt::{prime::largest_prime_in_arithmetic_progression64, *};
 use criterion::*;
@@ -12,107 +17,80 @@ impl<T: Copy + Add<Output = T> + Sub<Output = T> + Mul<Output = T>> Scalar for T
     }
 }
 
-// we can use `+`, `-`, `*` instead of `wrapping_*` because benches are always
-// run in release mode
-fn slice_wrapping_add<T: Scalar>(output: &mut [T], lhs: &[T], rhs: &[T]) {
-    output
-        .iter_mut()
-        .zip(lhs.iter().zip(rhs.iter()))
-        .for_each(|(out, (&lhs, &rhs))| *out = lhs + rhs);
-}
-fn slice_wrapping_sub<T: Scalar>(output: &mut [T], lhs: &[T], rhs: &[T]) {
-    output
-        .iter_mut()
-        .zip(lhs.iter().zip(rhs.iter()))
-        .for_each(|(out, (&lhs, &rhs))| *out = lhs - rhs);
-}
-fn slice_wrapping_add_assign<T: Scalar>(lhs: &mut [T], rhs: &[T]) {
-    lhs.iter_mut()
-        .zip(rhs.iter())
-        .for_each(|(lhs, &rhs)| *lhs = *lhs + rhs);
-}
-fn slice_wrapping_sub_assign<T: Scalar>(lhs: &mut [T], rhs: &[T]) {
-    lhs.iter_mut()
-        .zip(rhs.iter())
-        .for_each(|(lhs, &rhs)| *lhs = *lhs - rhs);
+#[derive(Serialize)]
+enum PrimeModulus {
+    // 32 bits section
+    FitsIn30Bits,
+    FitsIn31Bits,
+    FitsIn32Bits,
+    Native32,
+    // 64 bits section
+    FitsIn50Bits,
+    FitsIn51Bits,
+    FitsIn52Bits,
+    FitsIn62Bits,
+    FitsIn63Bits,
+    FitsIn64Bits,
+    Native64,
+    // 128 bits section
+    Native128,
 }
 
-fn polynomial_karatsuba_wrapping_mul<T: Scalar>(output: &mut [T], p: &[T], q: &[T]) {
-    // check same dimensions
-    let poly_size = output.len();
-
-    // allocate slices for the rec
-    let mut a0 = vec![T::zero(); poly_size];
-    let mut a1 = vec![T::zero(); poly_size];
-    let mut a2 = vec![T::zero(); poly_size];
-    let mut input_a2_p = vec![T::zero(); poly_size / 2];
-    let mut input_a2_q = vec![T::zero(); poly_size / 2];
-
-    // prepare for splitting
-    let bottom = 0..(poly_size / 2);
-    let top = (poly_size / 2)..poly_size;
-
-    // induction
-    induction_karatsuba(&mut a0, &p[bottom.clone()], &q[bottom.clone()]);
-    induction_karatsuba(&mut a1, &p[top.clone()], &q[top.clone()]);
-    slice_wrapping_add(&mut input_a2_p, &p[bottom.clone()], &p[top.clone()]);
-    slice_wrapping_add(&mut input_a2_q, &q[bottom.clone()], &q[top.clone()]);
-    induction_karatsuba(&mut a2, &input_a2_p, &input_a2_q);
-
-    // rebuild the result
-    slice_wrapping_sub(output, &a0, &a1);
-    slice_wrapping_sub_assign(&mut output[bottom.clone()], &a2[top.clone()]);
-    slice_wrapping_add_assign(&mut output[bottom.clone()], &a0[top.clone()]);
-    slice_wrapping_add_assign(&mut output[bottom.clone()], &a1[top.clone()]);
-    slice_wrapping_add_assign(&mut output[top.clone()], &a2[bottom.clone()]);
-    slice_wrapping_sub_assign(&mut output[top.clone()], &a0[bottom.clone()]);
-    slice_wrapping_sub_assign(&mut output[top], &a1[bottom]);
-}
-
-const KARATUSBA_STOP: usize = 64;
-/// Compute the induction for the karatsuba algorithm.
-fn induction_karatsuba<T: Scalar>(res: &mut [T], p: &[T], q: &[T]) {
-    // stop the induction when polynomials have KARATUSBA_STOP elements
-    if p.len() <= KARATUSBA_STOP {
-        // schoolbook algorithm
-        pulp::Arch::new().dispatch(
-            #[inline(always)]
-            || {
-                for (lhs_degree, &lhs_elt) in p.iter().enumerate() {
-                    let res = &mut res[lhs_degree..];
-                    for (&rhs_elt, res) in q.iter().zip(res) {
-                        *res = *res + lhs_elt * rhs_elt
-                    }
-                }
-            },
-        );
-    } else {
-        let poly_size = res.len();
-
-        // allocate slices for the rec
-        let mut a0 = vec![T::zero(); poly_size / 2];
-        let mut a1 = vec![T::zero(); poly_size / 2];
-        let mut a2 = vec![T::zero(); poly_size / 2];
-        let mut input_a2_p = vec![T::zero(); poly_size / 4];
-        let mut input_a2_q = vec![T::zero(); poly_size / 4];
-
-        // prepare for splitting
-        let bottom = 0..(poly_size / 4);
-        let top = (poly_size / 4)..(poly_size / 2);
-
-        // rec
-        induction_karatsuba(&mut a0, &p[bottom.clone()], &q[bottom.clone()]);
-        induction_karatsuba(&mut a1, &p[top.clone()], &q[top.clone()]);
-        slice_wrapping_add(&mut input_a2_p, &p[bottom.clone()], &p[top.clone()]);
-        slice_wrapping_add(&mut input_a2_q, &q[bottom], &q[top]);
-        induction_karatsuba(&mut a2, &input_a2_p, &input_a2_q);
-
-        // rebuild the result
-        slice_wrapping_sub(&mut res[(poly_size / 4)..(3 * poly_size / 4)], &a2, &a0);
-        slice_wrapping_sub_assign(&mut res[(poly_size / 4)..(3 * poly_size / 4)], &a1);
-        slice_wrapping_add_assign(&mut res[0..(poly_size / 2)], &a0);
-        slice_wrapping_add_assign(&mut res[(poly_size / 2)..poly_size], &a1);
+impl PrimeModulus {
+    fn from_u64(p: u64) -> Self {
+        if p <= 1 << 30 {
+            Self::FitsIn30Bits
+        } else if p <= 1 << 31 {
+            Self::FitsIn31Bits
+        } else if p <= 1 << 32 {
+            Self::FitsIn32Bits
+        } else if p <= 1 << 50 {
+            Self::FitsIn50Bits
+        } else if p <= 1 << 51 {
+            Self::FitsIn51Bits
+        } else if p <= 1 << 52 {
+            Self::FitsIn52Bits
+        } else if p <= 1 << 62 {
+            Self::FitsIn62Bits
+        } else if p <= 1 << 63 {
+            Self::FitsIn63Bits
+        } else {
+            Self::FitsIn64Bits
+        }
     }
+}
+
+#[derive(Serialize)]
+struct BenchmarkParametersRecord {
+    display_name: String,
+    polynomial_size: usize,
+    prime_modulus: PrimeModulus,
+    // If this field value is set to 0 means that the number is not a prime.
+    prime_number: u64,
+}
+
+/// Writes benchmarks parameters to disk in JSON format.
+fn write_to_json(
+    bench_id: &str,
+    display_name: impl Into<String>,
+    polynomial_size: usize,
+    prime_modulus: PrimeModulus,
+    prime_number: u64,
+) {
+    let record = BenchmarkParametersRecord {
+        display_name: display_name.into(),
+        polynomial_size,
+        prime_modulus,
+        prime_number,
+    };
+
+    let mut params_directory = ["benchmarks_parameters", bench_id]
+        .iter()
+        .collect::<PathBuf>();
+    fs::create_dir_all(&params_directory).unwrap();
+    params_directory.push("parameters.json");
+
+    fs::write(params_directory, serde_json::to_string(&record).unwrap()).unwrap();
 }
 
 fn criterion_bench(c: &mut Criterion) {
@@ -124,14 +102,20 @@ fn criterion_bench(c: &mut Criterion) {
             largest_prime_in_arithmetic_progression64(1 << 16, 1, 1 << 30, 1 << 31).unwrap(),
             largest_prime_in_arithmetic_progression64(1 << 16, 1, 1 << 31, 1 << 32).unwrap(),
         ] {
+            let p_u64 = p;
             let p = p as u32;
             let plan = prime32::Plan::try_new(n, p).unwrap();
-            c.bench_function(&format!("fwd-32-{p}-{n}"), |b| {
+            let bench_id = format!("fwd-32-{p}-{n}");
+            c.bench_function(&bench_id, |b| {
                 b.iter(|| plan.fwd(&mut data));
             });
-            c.bench_function(&format!("inv-32-{p}-{n}"), |b| {
+            write_to_json(&bench_id, "fwd-32", n, PrimeModulus::from_u64(p_u64), p_u64);
+
+            let bench_id = format!("inv-32-{p}-{n}");
+            c.bench_function(&bench_id, |b| {
                 b.iter(|| plan.inv(&mut data));
             });
+            write_to_json(&bench_id, "inv-32", n, PrimeModulus::from_u64(p_u64), p_u64);
         }
     }
 
@@ -146,12 +130,17 @@ fn criterion_bench(c: &mut Criterion) {
             largest_prime_in_arithmetic_progression64(1 << 16, 1, 1 << 63, u64::MAX).unwrap(),
         ] {
             let plan = prime64::Plan::try_new(n, p).unwrap();
-            c.bench_function(&format!("fwd-64-{p}-{n}"), |b| {
+            let bench_id = format!("fwd-64-{p}-{n}");
+            c.bench_function(&bench_id, |b| {
                 b.iter(|| plan.fwd(&mut data));
             });
-            c.bench_function(&format!("inv-64-{p}-{n}"), |b| {
+            write_to_json(&bench_id, "fwd-64", n, PrimeModulus::from_u64(p), p);
+
+            let bench_id = format!("inv-64-{p}-{n}");
+            c.bench_function(&bench_id, |b| {
                 b.iter(|| plan.inv(&mut data));
             });
+            write_to_json(&bench_id, "inv-64", n, PrimeModulus::from_u64(p), p);
         }
     }
 
@@ -161,30 +150,36 @@ fn criterion_bench(c: &mut Criterion) {
         let rhs = vec![0; n];
 
         let plan = native32::Plan32::try_new(n).unwrap();
-        c.bench_function(&format!("native32-32-{n}"), |b| {
+        let bench_id = format!("native32-32-{n}");
+        c.bench_function(&bench_id, |b| {
             b.iter(|| plan.negacyclic_polymul(&mut prod, &lhs, &rhs));
         });
+        write_to_json(&bench_id, "native32-32", n, PrimeModulus::Native32, 0);
+
         let plan = native_binary32::Plan32::try_new(n).unwrap();
-        c.bench_function(&format!("nativebinary32-32-{n}"), |b| {
+        let bench_id = format!("nativebinary32-32-{n}");
+        c.bench_function(&bench_id, |b| {
             b.iter(|| plan.negacyclic_polymul(&mut prod, &lhs, &rhs));
         });
+        write_to_json(&bench_id, "nativebinary32-32", n, PrimeModulus::Native32, 0);
 
         #[cfg(feature = "nightly")]
         {
             if let Some(plan) = native32::Plan52::try_new(n) {
-                c.bench_function(&format!("native32-52-{n}"), |b| {
+                let bench_id = format!("native32-52-{n}");
+                c.bench_function(&bench_id, |b| {
                     b.iter(|| plan.negacyclic_polymul(&mut prod, &lhs, &rhs));
                 });
+                write_to_json(&bench_id, "native32-52", n, PrimeModulus::Native32, 0);
             }
             if let Some(plan) = native_binary32::Plan52::try_new(n) {
-                c.bench_function(&format!("nativebinary32-52-{n}"), |b| {
+                let bench_id = format!("nativebinary32-52-{n}");
+                c.bench_function(&bench_id, |b| {
                     b.iter(|| plan.negacyclic_polymul(&mut prod, &lhs, &rhs));
                 });
+                write_to_json(&bench_id, "nativebinary32-52", n, PrimeModulus::Native32, 0);
             }
         }
-        c.bench_function(&format!("native32-karatsuba-{n}"), |b| {
-            b.iter(|| polynomial_karatsuba_wrapping_mul(&mut prod, &lhs, &rhs));
-        });
     }
 
     for n in ns {
@@ -193,30 +188,36 @@ fn criterion_bench(c: &mut Criterion) {
         let rhs = vec![0; n];
 
         let plan = native64::Plan32::try_new(n).unwrap();
-        c.bench_function(&format!("native64-32-{n}"), |b| {
+        let bench_id = format!("native64-32-{n}");
+        c.bench_function(&bench_id, |b| {
             b.iter(|| plan.negacyclic_polymul(&mut prod, &lhs, &rhs));
         });
+        write_to_json(&bench_id, "native64-32", n, PrimeModulus::Native64, 0);
+
         let plan = native_binary64::Plan32::try_new(n).unwrap();
-        c.bench_function(&format!("nativebinary64-32-{n}"), |b| {
+        let bench_id = format!("nativebinary64-32-{n}");
+        c.bench_function(&bench_id, |b| {
             b.iter(|| plan.negacyclic_polymul(&mut prod, &lhs, &rhs));
         });
+        write_to_json(&bench_id, "nativebinary64-32", n, PrimeModulus::Native64, 0);
 
         #[cfg(feature = "nightly")]
         {
             if let Some(plan) = native64::Plan52::try_new(n) {
-                c.bench_function(&format!("native64-52-{n}"), |b| {
+                let bench_id = format!("native64-52-{n}");
+                c.bench_function(&bench_id, |b| {
                     b.iter(|| plan.negacyclic_polymul(&mut prod, &lhs, &rhs));
                 });
+                write_to_json(&bench_id, "native64-52", n, PrimeModulus::Native64, 0);
             }
             if let Some(plan) = native_binary64::Plan52::try_new(n) {
-                c.bench_function(&format!("nativebinary64-52-{n}"), |b| {
+                let bench_id = format!("nativebinary64-52-{n}");
+                c.bench_function(&bench_id, |b| {
                     b.iter(|| plan.negacyclic_polymul(&mut prod, &lhs, &rhs));
                 });
+                write_to_json(&bench_id, "nativebinary64-52", n, PrimeModulus::Native64, 0);
             }
         }
-        c.bench_function(&format!("native64-karatsuba-{n}"), |b| {
-            b.iter(|| polynomial_karatsuba_wrapping_mul(&mut prod, &lhs, &rhs));
-        });
     }
 
     for n in ns {
@@ -225,16 +226,24 @@ fn criterion_bench(c: &mut Criterion) {
         let rhs = vec![0; n];
 
         let plan = native128::Plan32::try_new(n).unwrap();
-        c.bench_function(&format!("native128-32-{n}"), |b| {
+        let bench_id = format!("native128-32-{n}");
+        c.bench_function(&bench_id, |b| {
             b.iter(|| plan.negacyclic_polymul(&mut prod, &lhs, &rhs));
         });
+        write_to_json(&bench_id, "native128-32", n, PrimeModulus::Native128, 0);
+
         let plan = native_binary128::Plan32::try_new(n).unwrap();
-        c.bench_function(&format!("nativebinary128-32-{n}"), |b| {
+        let bench_id = format!("nativebinary128-32-{n}");
+        c.bench_function(&bench_id, |b| {
             b.iter(|| plan.negacyclic_polymul(&mut prod, &lhs, &rhs));
         });
-        c.bench_function(&format!("native128-karatsuba-{n}"), |b| {
-            b.iter(|| polynomial_karatsuba_wrapping_mul(&mut prod, &lhs, &rhs));
-        });
+        write_to_json(
+            &bench_id,
+            "nativebinary128-32",
+            n,
+            PrimeModulus::Native128,
+            0,
+        );
     }
 }
 
